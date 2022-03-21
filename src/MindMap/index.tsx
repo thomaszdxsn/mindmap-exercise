c0f2a8550f05081aecd6bdc8cb0c7e54eb8d1328
thomaszdxsn <thomaszdxsn@icloud.com>
2022-03-21 17:51:27 +0800

    :sparkles: add error message for invalid input

--- src/MindMap/index.tsx
+++ src/MindMap/index.tsx
@@ -1,4 +1,8 @@
+import classNames from "classnames";
 import { useRef } from "react";
+import { useRecoilValue } from "recoil";
+import { translateInvalidTextEnum } from "../OutlineEditor/utils";
+import { nodesSelector } from "../states";
 import { useZoomTransformState, buildSVGTransformAttr } from "./hooks";
 
 interface Props {
@@ -19,4 +23,30 @@ function MindMap({ className }: Props) {
   );
 }
 
-export default MindMap;
+function Errors(props: Props) {
+  const error = useRecoilValue(nodesSelector).error!;
+  const message = translateInvalidTextEnum(error.error);
+  const className = classNames(
+    props.className,
+    "flex justify-center items-center",
+    "flex-col gap-2",
+    "text-red-300"
+  );
+  return (
+    <div className={className}>
+      <div>
+        <div>行数：{error.lineNum + 1}</div>
+        <div>错误：{message}</div>
+      </div>
+    </div>
+  );
+}
+
+function Wrapper(props: Props) {
+  const data = useRecoilValue(nodesSelector);
+  const Component = data.ok ? MindMap : Errors;
+
+  return <Component {...props} />;
+}
+
+export default Wrapper;
